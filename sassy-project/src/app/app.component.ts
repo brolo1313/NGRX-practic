d58babc9ea67dd70a3c4f79b9af1ab84a539f201
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-11-16 10:03:16 +0200

    NGRX-1 counter add

--- sassy-project/src/app/app.component.ts
+++ sassy-project/src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sassy-project';
+  counter = 0;
+  updateAt?:number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updateAt = Date.now();
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updateAt = Date.now();
+    this.counter--;
+  }
+
+  clear(): void {
+    this.updateAt = Date.now();
+    this.counter = 0;
+  }
 }
